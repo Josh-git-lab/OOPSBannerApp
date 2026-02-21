a44904d637d4cd2bb08c9d1436e842ddc49ecde5
Josh-git-lab <joshjiby3030@gmail.com>
2026-02-21 21:35:42 +0530

    Prints OOPS Banner using string.join()

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,20 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", "*****", "*****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*   *", "*    "));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*****", "*****"));
+
+        System.out.println(String.join(" ",
+                "*   *", "*   *", "*    ", "    *"));
+
+        System.out.println(String.join(" ",
+                " *** ", " *** ", "*    ", "*****"));
+    }
+}
